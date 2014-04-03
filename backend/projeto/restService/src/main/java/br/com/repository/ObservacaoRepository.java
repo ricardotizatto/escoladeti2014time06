ed83f89142de1d68266961403241137e4001eb5b
Jhonatan Catabriga <jhonatancatabriga@gmail.com>
2014-04-02 22:34:54 -0300

    2463 criar estrutura básica do projeto

---
+++ backend/projeto/restService/src/main/java/br/com/repository/ObservacaoRepository.java
@@ -0,0 +1,9 @@
+package br.com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.entity.Observacao;
+
+public interface ObservacaoRepository extends JpaRepository<Observacao, Long>{
+
+}
