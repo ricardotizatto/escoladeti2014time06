9878e31dd91b8ccebdb3a5e529e90dca48da0ff7
martinho.omura <martinhomassao@gmail.com>
2014-04-10 17:49:13 -0300

    #2565 Criação de Controllers De Cadastro de Estado e Lista de Estado.

---
+++ frontend/Telas/Lib/js/ListaEstadoController.js
@@ -0,0 +1,12 @@
+function ListaEstadoController ($scope, bd) {
+  $scope.estados = bd.estados;
+
+  $scope.editarEstado = function( indice ){
+    bd.estado = $scope.estados[indice];
+    window.location = "#/cadastroestado";  
+  };
+
+  $scope.delEstado= function(index){
+      $scope.estados.splice(index,1);
+  }; 
+}
