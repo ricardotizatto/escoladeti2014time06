ebbe49ad64fce29279064304750d707058a7ff80
unknown <walberjr.oliveira@gmail.com>
2014-04-15 14:47:53 -0300

    #2615 - Correção no botão Novo

--- frontend/Telas/Lib/js/ListaPerfilUsuarioController.js
+++ frontend/Telas/Lib/js/ListaPerfilUsuarioController.js
@@ -10,7 +10,7 @@ function listaPerfilUsuarioController ($scope, bd) {
 	}
 
 	$scope.novoPerfilUsuario = function() {
-		bd.perfilUsuario = {};
+		bd.perfilDeUsuario = {};
 		window.location = '#/cadastroperfilusuario';
 	}
 	
