a07981ffce480d930db967d0b9d97a555ec06ffc
martinho.omura <martinhomassao@hotmail.com>
2014-04-15 20:52:51 -0300

    #2621 correções de bugs

--- frontend/Telas/Lib/js/CadastroBairroController.js
+++ frontend/Telas/Lib/js/CadastroBairroController.js
@@ -7,10 +7,11 @@ function CadastroBairroController( $scope, bd ){
     { nome : "Sarandi"  }
   ];
 
-  $scope.faixasCeps[
-    { faixa : "01000-000 - 02000-000" },
-    { faixa : "02001-000 - 03000-000" },
-    { faixa : "03001-000 - 04001-000" }
+  $scope.faixasCeps = [
+    { faixaCep : "01000-000 - 02000-000" },
+    { faixaCep : "02001-000 - 03000-000" },
+    { faixaCep : "03001-000 - 04001-000" },
+    { faixaCep : "04001-000 - 05001-000" }
   ];
 
   $scope.bairro = bd.bairro || {};
@@ -21,6 +22,7 @@ function CadastroBairroController( $scope, bd ){
   $scope.salvarBairro = function(){
     if(!$scope.bairro.idBairro){
         $scope.idBairro++;
+        $scope.bairro.idBairro = $scope.idBairro;
         $scope.bairros.push( $scope.bairro );
         bd.bairros.push($scope.bairro);
     }
