4d7f95be12e76a4a62833ae74a9d98b1e8fbbab6
winicius <winiciuswanderlei@gmail.com>
2014-05-12 19:33:44 -0300

    #2709 Alterado CidadeController

---
+++ backend/projeto/escoladeti/src/main/java/br/unicesumar/escoladeti/controller/CidadeController.java
@@ -0,0 +1,48 @@
+
+package br.unicesumar.escoladeti.controller;
+
+import br.unicesumar.escoladeti.entity.Cidade;
+import br.unicesumar.escoladeti.service.CidadeService;
+import java.io.Serializable;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/rest/cidadeSource")
+public class CidadeController implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Autowired
+    private CidadeService cidadeService;
+    
+    @RequestMapping(value="/cidade", method = RequestMethod.POST)
+    @ResponseBody
+    
+    public Cidade salvar(@RequestBody Cidade cidade){
+        return cidadeService.salvar(cidade);
+    }
+    
+    @RequestMapping(value="/cidade/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Cidade getById(@PathVariable Long id){
+        return cidadeService.getById(id);
+    }
+    
+    @RequestMapping(value="/pais", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Cidade> getTodos(){
+        return cidadeService.getTodos();
+    }
+    
+    @RequestMapping(value="/pais", method = RequestMethod.DELETE)
+    @ResponseBody
+    public String deletar(@RequestBody Cidade cidade){
+        cidadeService.deletar(cidade);
+        return "deleted";
+    }
+}
