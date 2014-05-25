b27d658e906daac8c881f1faad604b6a33642fab
Walber Oliveira <walberjr.oliveira@gmail.com>
2014-05-25 19:49:33 -0300

    #2923 e #2893 - Backend pronto e implementação frontend

---
+++ backend/projeto/escoladeti/src/main/java/br/unicesumar/escoladeti/controller/UsuarioController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.unicesumar.escoladeti.controller;
+
+import br.unicesumar.escoladeti.entity.Usuario;
+import br.unicesumar.escoladeti.service.UsuarioService;
+import java.io.Serializable;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author Diogo
+ */
+@Controller
+@RequestMapping("/rest/usuarioSource")
+public class UsuarioController implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Autowired
+    private UsuarioService usuarioService;
+    
+    @RequestMapping(value = "/usuario", method = RequestMethod.POST)
+    @ResponseBody
+    public Usuario salvar(@RequestBody Usuario usuario){
+        return this.usuarioService.salvar(usuario);
+    }
+    
+    @RequestMapping(value = "/usuario/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Usuario getById(@PathVariable Long id){
+        return this.usuarioService.getById(id);
+    }
+    
+    @RequestMapping(value = "/usuario", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Usuario> getTodos(){
+        return this.usuarioService.getTodos();
+    }
+    
+    @RequestMapping(value = "/usuario", method = RequestMethod.DELETE)
+    @ResponseBody
+    public String deletar(@RequestBody Usuario usuario){
+        this.usuarioService.deletar(usuario);
+        return "deleted";
+    }
+}
