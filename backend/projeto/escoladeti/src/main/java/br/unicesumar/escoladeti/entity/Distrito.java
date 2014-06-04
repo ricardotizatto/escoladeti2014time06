fc3290dbc363af36680f774fcc9675e307be2b54
winicius.rosa <winicius92@gmail.com>
2014-06-04 11:46:08 -0300

    #3116 Criação backend

--- backend/projeto/escoladeti/src/main/java/br/unicesumar/escoladeti/entity/Distrito.java
+++ backend/projeto/escoladeti/src/main/java/br/unicesumar/escoladeti/entity/Distrito.java
@@ -1,29 +1,51 @@
 package br.unicesumar.escoladeti.entity;
 
 import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
 import javax.validation.constraints.NotNull;
 
 @Entity
-public class Distrito extends Entidade{
-	@Column(nullable=false)
+public class Distrito extends Entidade {
+
+    @Column(nullable = false)
+    @NotNull
+    @Temporal(javax.persistence.TemporalType.DATE)
     private Date inicioVigencia;
-	@Column(nullable=false)
-    private Date fimVigencia;
-	
-	@OneToOne
-	@JoinColumn(name="id_distrito", nullable=false)
-	private Cidade distrito;
     
+    @Column(nullable = false)
+    @NotNull
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fimVigencia;
+
+    @OneToOne
+    @JoinColumn(name = "id_distrito", nullable = false)
+    private Cidade distrito;
+
     @ManyToOne
-    @JoinColumn(name="id_cidade", nullable =false)
-    private Cidade cidadeSede;
+    @JoinColumn(name = "id_cidade", nullable = false)
+    private Cidade municipio;
+    
+    public Distrito(){
+    }
+    public Distrito(Date inicioVigencia, Date fimVigencia, Cidade distrito, Cidade municipio){
+        this.inicioVigencia = inicioVigencia;
+        this.fimVigencia = fimVigencia;
+        this.distrito = distrito;
+        this.municipio = municipio;
+    }
+    public Distrito(Long id, Date inicioVigencia, Date fimVigencia, Cidade distrito, Cidade municipio){
+        this.id = id;
+        this.inicioVigencia = inicioVigencia;
+        this.fimVigencia = fimVigencia;
+        this.distrito = distrito;
+        this.municipio = municipio;
+    }
     
     public Date getInicioVigencia() {
         return inicioVigencia;
@@ -42,10 +64,10 @@ public class Distrito extends Entidade{
     }
 
     public Cidade getCidadeSede() {
-        return cidadeSede;
+        return municipio;
     }
 
-    public void setCidadeSede(Cidade cidadeSede) {
-        this.cidadeSede = cidadeSede;
+    public void setCidadeSede(Cidade municipio) {
+        this.municipio = municipio;
     }
 }
