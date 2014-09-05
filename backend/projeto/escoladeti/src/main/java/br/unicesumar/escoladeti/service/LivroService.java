bdebc44008bcf47b179d190692eae10859e2cb39
unknown <jr.ferrugem@hotmail.com>
2014-09-04 22:35:46 -0300

    #3838 - Finalizado edicao livro

--- backend/projeto/escoladeti/src/main/java/br/unicesumar/escoladeti/service/LivroService.java
+++ backend/projeto/escoladeti/src/main/java/br/unicesumar/escoladeti/service/LivroService.java
@@ -4,6 +4,7 @@ import br.unicesumar.escoladeti.controller.DataPage;
 import static br.unicesumar.escoladeti.controller.DataPage.pageRequestForAsc;
 import br.unicesumar.escoladeti.entity.Livro;
 import br.unicesumar.escoladeti.repository.LivroRepository;
+import br.unicesumar.escoladeti.util.number.NumberUtils;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,19 @@ public class LivroService {
     @Autowired
     private LivroRepository livroRepository;
 
-    public Livro salvar(Livro livro) {
-        return this.livroRepository.save(livro);
-    }
+    public Livro salvar(Livro livro) throws Exception {
 
-    public DataPage<Livro> getTodos(Integer pagina){
+        if (NumberUtils.isYearValid(livro.getAnoEdicao())) {
+            if (!livro.equals(buscarLivroPorNomeAutorEditoraAnoEdicao(livro))) {
+                return this.livroRepository.save(livro);
+            }
+            throw new Exception("O Livro " + livro.getNome() + " já está cadastrado!");
+        }
+        throw new Exception("O Ano não pode ser maior que o atual");
+
+    //return this.livroRepository.save (livro);
+}
+public DataPage<Livro> getTodos(Integer pagina){
         return new DataPage<>(livroRepository.findAll(pageRequestForAsc(pagina, "nome")));
     }
     
@@ -37,6 +46,7 @@ public class LivroService {
     public Livro getById(Long id) {
         return this.livroRepository.findById(id);
     }
+    
 
     public Livro buscarLivroPorNomeAutorEditoraAnoEdicao(Livro livro) {
         return this.livroRepository.findByNomeAndAutorAndEditoraAndAnoEdicao(
