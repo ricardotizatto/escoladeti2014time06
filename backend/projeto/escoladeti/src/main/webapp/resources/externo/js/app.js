961e1b0c32fc1ace2db7e8c082488c22e4a158cd
martinho.omura <martinhomassao@gmail.com>
2014-10-20 17:50:45 -0200

    #4273 - Montagens das páginas externas no projeto

--- backend/projeto/escoladeti/src/main/webapp/resources/externo/js/app.js
+++ backend/projeto/escoladeti/src/main/webapp/resources/externo/js/app.js
@@ -1,11 +1,16 @@
 angular.module('appExterno', [])
-    .controller('AppCtrontroller', AppCtrontroller);
+    .controller('AppCtrontroller', 
+                '$routeParams',
+                '$http',
+                'AppFactory',
+                AppCtrontroller);
 
-
-function AppCtrontroller($http) {
+function AppCtrontroller($routeParams,$http) {
+    this.routeParams = $routeParams;
     this.http = $http;
-
     this.iniciar();
+    
+    this.ativa = "home";
 }
 
 AppCtrontroller.prototype = {
@@ -17,6 +22,65 @@ AppCtrontroller.prototype = {
             .success(function (eventos) {
                self.eventos = eventos;
             });
+    }, 
+    paginaAtiva: function (pagina) {
+        var self = this;        
+        self.ativa = 'home';
     }
+            
 };
+
+angular.module('controllers', ['services']);
+angular.module('services', []);
+
+var appExterno = angular.module('appExterno',
+        ['ngRoute',
+        'ngResource',
+        'controllers']); 
+
+appExterno.config(['$routeProvider',
+    function ($routeProvider) {
+
+        $routeProvider
+            .when('/principal', {
+                templateUrl: './paginasexternas/principal.html',
+                //controller: 'ExternoController'
+            })
+            .when('/eventos', {
+                templateUrl: './paginasexternas/eventos.html',
+                //controller: 'ExternoController'
+            })
+            .when('/detalhes-evento/:eventoId', {
+                templateUrl: './paginasexternas/detalhes-evento.html',
+                //controller: 'ExternoController'
+            })
+            .when('/detalhes-evento', {
+                templateUrl: './paginasexternas/detalhes-evento.html',
+                //controller: 'ExternoController'
+            })
+            .when('/evento-realizado/:eventoId', {
+                templateUrl: './paginasexternas/evento-realizado.html',
+                //controller: 'ExternoController'
+            })
+            .when('/evento-realizado', {
+                templateUrl: './paginasexternas/evento-realizado.html',
+                //controller: 'ExternoController'
+            })
+            .when('/transcricoes', {
+                templateUrl: './paginasexternas/transcricoes.html',
+                //controller: 'ExternoController'
+            })
+            .when('/sobre', {
+                templateUrl: './paginasexternas/sobre.html',
+                //controller: 'ExternoController'
+            })
+            .when('/localizacao', {
+                templateUrl: './paginasexternas/localizacao.html',
+                //controller: 'ExternoController'
+            })
+            .otherwise({redirectTo: '/principal'
+            });
+    }
+]);
+
 
