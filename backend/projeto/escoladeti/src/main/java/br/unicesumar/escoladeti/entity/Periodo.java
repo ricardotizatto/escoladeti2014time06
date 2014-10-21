d732b725e49791689759126c2120e8fefde764e0
leivid <leividm@gmail.com>
2014-10-20 22:06:43 -0200

    #4283 - Correção no save dos periodos com evento.

--- backend/projeto/escoladeti/src/main/java/br/unicesumar/escoladeti/entity/Periodo.java
+++ backend/projeto/escoladeti/src/main/java/br/unicesumar/escoladeti/entity/Periodo.java
@@ -3,6 +3,7 @@ package br.unicesumar.escoladeti.entity;
 import br.unicesumar.escoladeti.util.data.DateUtil;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import java.util.Date;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -59,4 +60,39 @@ public class Periodo extends Entidade{
     public boolean validaData() {
         return DateUtil.validBeforeDate(this.data);
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.data);
+        hash = 37 * hash + Objects.hashCode(this.inicio);
+        hash = 37 * hash + Objects.hashCode(this.fim);
+        hash = 37 * hash + Objects.hashCode(this.evento);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Periodo other = (Periodo) obj;
+        if (!Objects.equals(this.data, other.data)) {
+            return false;
+        }
+        if (!Objects.equals(this.inicio, other.inicio)) {
+            return false;
+        }
+        if (!Objects.equals(this.fim, other.fim)) {
+            return false;
+        }
+        if (!Objects.equals(this.evento, other.evento)) {
+            return false;
+        }
+        return true;
+    }
+    
 }
